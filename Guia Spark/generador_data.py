6d656390272f2cb8ba98d42de6ebcc2dd7989d95
Nicolas Aguerre <naguerre@fi.uba.ar>
2019-10-06 00:18:14 -0300

    Agregado Ejercicio 3

--- Guia Spark/generador_data.py
+++ Guia Spark/generador_data.py
@@ -3,5 +3,5 @@ import random
 categorias = ["Comida","Tocador","Computacion","Bazar","Muebles"]
 
 def generar_data(cantidad):
-	for i in range(1,200+1):
-		print(f"{i},Descripcion Random,{random.choice(categorias)}")
+	for i in range(cantidad):
+		print(f"{random.randint(1,28)}-{random.randint(1,12)}-{random.randint(2017,2019)},{random.randint(1,200)},{random.randint(500,2000)}")
